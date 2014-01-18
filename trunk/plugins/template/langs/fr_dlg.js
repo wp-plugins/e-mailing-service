0538e1494982deb4fa6387b452393f2c2ffb5117
jooky <jooky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-18 23:28:41 +0000

    Remove accidentally-added tags/2.0
    git-svn-id: https://plugins.svn.wordpress.org/e-mailing-service/tags/4.2@841087 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/plugins/template/langs/fr_dlg.js
@@ -0,0 +1,15 @@
+tinyMCE.addI18n('fr.template_dlg',{
+title:"Modeles",
+label:"Modele",
+desc_label:"Description",
+desc:"Inserer le contenu d'un modele predefini",
+select:"Selectionner un modele",
+preview:"Aperçu",
+warning:"Attention ! Mettre a jour un modele avec un autre peut provoquer une perte de donnees.",
+mdate_format:"%d-%m-%Y %H:%M:%S",
+cdate_format:"%d-%m-%Y %H:%M:%S",
+months_long:"Janvier,Fevrier,Mars,Avril,Mai,Juin,Juillet,Août,Septembre,Octobre,Novembre,Decembre",
+months_short:"Janv,Fevr,Mars,Avr,Mai,Juin,Juil,Aout,Sept,Oct,Nov,Dec",
+day_long:"Dimanche,Lundi,Mardi,Mercredi,Jeudi,Vendredi,Samedi,Dimanche",
+day_short:"Dim,Lun,Mar,Mer,Jeu,Ven,Sam,Dim"
+});
---
+++ trunk/unsuscribe.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); 
+global $wpbd;
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php 
+			  
+  global $wp_query;
+  $smhie= $wp_query->query_vars['smhie'];
+  $smemail= $wp_query->query_vars['smemail'];
+  $smfree= $wp_query->query_vars['smfree'];
+  $smemail=str_replace('%5BZ%5D','@',$smemail);
+  $smemail=str_replace('[Z]','@',$smemail);
+  if($smfree=='1'){
+    $email=affiche_mail($smhie,$smemail);
+  } else {
+	$email_id = $_SESSION["sm_emailid"];
+	$smhie = $_SESSION["sm_hie"];
+	$email = affiche_mail($smhie,$email_id); 
+	$smemail=$email_id;
+  }
+  if(isset($_POST["action"])){
+  if($_POST["action"] == "update_desinscrit"){
+  update_inscrit($_POST["smhie"],$_POST["email"],$_POST["email_id"]);
+  if($_POST["valide"]=="oui"){
+  echo get_option('sm_udp_merci');
+  } 
+  } 
+  } else {
+  echo '<br>
+  <form action="'.get_option('siteurl').'/upd/'.$smemail.'/'.$smhie.'/" method="post">
+                    <input name="email" type="hidden" value="'.$email.'">
+					<input name="email_id" type="hidden" value="'.$email_id.'">
+					<input name="smhie" type="hidden" value="'.$smhie.'">
+					<input name="action" type="hidden" value="update_desinscrit">
+					<pre>'.get_option('sm_udp_details').'</pre><br>
+					  <table width="200" border"0">
+				<tr><td><label><input type="radio" name="valide" value="oui"  checked>'.__('oui').'</label></td></tr>
+				<tr><td><label><input type="radio" name="valide" value="non">'.__('non').'</label></td></tr>
+				<tr><td><input name="sumit" type="submit" value="'.get_option('sm_udp_bouton').'"></td></tr>
+				      </table>					  
+					</form><br><br>'; 
+					
+		}
+		?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
