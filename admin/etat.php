7dc4c59fb04cda7ca2c4a2965f945316676ab2a4
jooky <jooky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 19:10:51 +0000

    tagging version 7.4
    git-svn-id: https://plugins.svn.wordpress.org/e-mailing-service/trunk@862541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/etat.php
+++ admin/etat.php
@@ -68,18 +68,24 @@ echo "<h1>".__("Status serveur SMTP","e-mailing-service")."</h1>";
     $tbaleau_insert .= '</tr>';
     }
     } else {
+	$num=1;
     $tbaleau_insert .= "<tr>
-	<td><blockquote>".get_option('sm_smtp_server')."</blockquote></td>
-	<td><blockquote>".gethostbyname(get_option('sm_smtp_server'))."</blockquote></td>
-	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server'),"25")."</blockquote></td>
-	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server'),"587")."</blockquote></td>
-	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server'),"80")."</blockquote></td>
-	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server'),"53")."</blockquote></td>
-	<td><blockquote>".sm_spamscore(get_option('sm_smtp_server'))."</blockquote></td>
-	<td><blockquote>".sm_blacklist(gethostbyname(get_option('sm_smtp_server')))."</blockquote></td>
-	<td><blockquote><span class=\"sm_table_vert\">&nbsp;&nbsp;".__("Actif")."&nbsp;&nbsp;</span></blockquote></td>";
+    <td><blockquote>".get_option('sm_smtp_server_'.$num.'')."</blockquote></td>
+	<td><blockquote>".gethostbyname(get_option('sm_smtp_server_'.$num.''))."</blockquote></td>
+	<td><blockquote><a href=\"?page=e-mailing-service/admin/etat.php&action=tester&num=".$num."\" target=\"_blank\">".__('Tester le serveur','e-mailing-service')."</a></blockquote></td>
+	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server_'.$num.''),"25")."</blockquote></td>
+	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server_'.$num.''),"587")."</blockquote></td>
+	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server_'.$num.''),"80")."</blockquote></td>
+	<td><blockquote>".sm_getStatus(get_option('sm_smtp_server_'.$num.''),"53")."</blockquote></td>
+	<td><blockquote>".sm_spamscore(get_option('sm_smtp_server_'.$num.''))."</blockquote></td>
+	<td><blockquote>".sm_blacklist(gethostbyname(get_option('sm_smtp_server_'.$num.'')))."</blockquote></td>";
+	if(get_option('sm_smtp_actif_'.$num.'') =="oui"){
+	$tbaleau_insert .= "<td><blockquote><span class=\"sm_table_vert\">&nbsp;&nbsp;".__("Actif")."&nbsp;&nbsp;</span></blockquote></td>";
+	} else {
+	$tbaleau_insert .= "<td><blockquote><span class=\"sm_table_rouge\">&nbsp;&nbsp;".__("Inactif")."&nbsp;&nbsp;</span></blockquote></td>";	
+	}
     $tbaleau_insert .= '</tr>';	   
-   }
+    }
 
 $tbaleau_insert .= '</tbody></table>';
 echo $tbaleau_insert ;
