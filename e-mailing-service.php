f215a7ac3191ed5922ae26d1f057cb167fab500a
jooky <jooky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-29 09:21:27 +0000

    tagging version 8.1
    git-svn-id: https://plugins.svn.wordpress.org/e-mailing-service/trunk@904659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- e-mailing-service.php
+++ e-mailing-service.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: e-mailing service
-Version: 7.9
+Version: 8.1
 Plugin URI: http://www.e-mailing-service.net
 Description: Send newsletters (emails) with wordpress. Detailed statistics AND rewritting on activation of the Free API
 Author URI: http://www.e-mailing-service.net
@@ -21,7 +21,7 @@ if ( is_plugin_active_for_network(plugin_basename(__FILE__)) ) {
 	$exit_msg = __('E-mailing service est deja installe', 'e-mailing-service');
 	exit($exit_msg);
 }
-define( 'smVERSION', '8.0' );
+define( 'smVERSION', '8.1' );
 define( 'smDBVERSION', '3.0' );
 define( 'smPATH', trailingslashit(dirname(__FILE__)) );
 define( 'smDIR', trailingslashit(dirname(plugin_basename(__FILE__))) );
